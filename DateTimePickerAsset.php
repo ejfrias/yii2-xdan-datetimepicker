24b7cf3d9f02e4b330300284425235763b337527
EJ Frias <ejsfrias@gmail.com>
2018-04-19 11:29:58 +0800

    Add files via upload

---
+++ DateTimePickerAsset.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ejfrias\yii2_xdan_datetimepicker;
+
+use Yii;
+use yii\web\JqueryAsset;
+
+class DateTimePickerAsset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@vendor/xdan/datetimepicker';
+
+    public $js = [
+        'jquery.datetimepicker.js',
+    ];
+
+    public $css = [
+        'jquery.datetimepicker.css',
+    ];
+
+    public function init()
+    {
+        parent::init();
+
+        $this->depends[] = JqueryAsset::className();
+    }
+
+
+}
